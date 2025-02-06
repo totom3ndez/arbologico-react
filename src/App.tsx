3043f8f1bb9b2d37b67ee7ef3fa42f3857240e44
totom3ndez <tomii.mendez45@gmail.com>
2025-02-06 23:09:01 +0100

    Moved button help action to another file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import { useState } from 'react'
 import './App.css'
+import { useState } from 'react'
 import { HelpIcon } from './assets'
 import { Button, ShowHelp, Form } from './components'
+import { showHelpScreen } from './actions'
 
 function App() {
   const [showHelp, setShowHelp] = useState(false)
-  const showHelpScreen = () => {
-    setShowHelp(!showHelp)
-  }
 
   return (
     <div className='app'>
-      <aside>
+      <aside className=''>
         <div className='flex gap-2 font-bold justify-center'>
-          <Button style='flex gap-2 text-center items-center bg-amber-200 hover:bg-amber-300 transition-all ease- rounded-full py-2 px-3' icon={<HelpIcon />} label='Como usar' method={showHelpScreen} />
+          <Button style='flex gap-2 text-center items-center bg-amber-200 hover:bg-amber-300 transition-all ease rounded-full py-2 px-3' icon={<HelpIcon />} label='Como usar' method={() => showHelpScreen(setShowHelp, showHelp)} />
         </div>
         <h2 className='font-bold text-xl'>Menú de acciones</h2>
-        <Form />
+        <Form submitAction={() => { }} />
       </aside>
       <main className='w-full'>
-        {showHelp && <ShowHelp method={showHelpScreen} />}
+        {showHelp && <ShowHelp method={() => showHelpScreen(setShowHelp, showHelp)} />}
       </main>
     </div>
   )
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,21 +1,28 @@
-export const Form = () => {
+interface Props {
+    submitAction: () => void
+}
+
+export const Form = ({ submitAction }: Props) => {
+    const inputStyle = "flex flex-col items-center bg-slate-300 rounded-full w-3/4"
+
     return (
-        <form className="flex flex-col gap-4 justify-center">
-            <input className="w-fit m-auto text-black placeholder:text-center focus:border-green-600 border-2 rounded-full p-2" type="text" name="name" placeholder="Nombre y apellido" />
-            <label htmlFor="alive">¿Está vivo?
-                <select name="alive">
+        <form className="flex flex-col gap-4 justify-center items-center" onSubmit={submitAction}>
+            <input autoComplete="true" className="w-fit m-auto bg-slate-300 text-black placeholder:text-center text-center rounded-full p-2" type="text" name="name" placeholder="Nombre y apellido" />
+            <label htmlFor="alive" className="bg-slate-300 w-fit p-2 rounded-full">¿Está vivo?
+                <select name="alive" id='alive'>
                     <option value="Si">Si</option>
                     <option value="No">No</option>
                 </select>
             </label>
-            <label htmlFor="dob">
+            <label htmlFor="dob" className={inputStyle}>
                 Fecha de nacimiento
-                <input type="date" name="dob" id="dob" />
+                <input type="date" name="dob" id="dob" autoComplete="true" />
             </label>
-            <label htmlFor="dod">
+            <label htmlFor="dod" className={inputStyle}>
                 Fecha de defunción
-                <input type="date" name="dod" id="dod" />
+                <input type="date" name="dod" id="dod" autoComplete="true" />
             </label>
+            <input type="submit" value="Enviar" className="bg-slate-300 rounded-full px-6 py-2" />
         </form>
     )
 }
