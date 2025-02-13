cc11c569bcc96503434972777657fb9aa189b62f
totom3ndez <tomii.mendez45@gmail.com>
2025-02-13 19:59:02 +0100

    Reviewed ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,25 @@ import { HelpIcon } from './assets'
 import { Button, ShowHelp, Form } from './components'
 import { showHelpScreen } from './actions'
 
+const buttonStyles = {
+  helpButton: 'flex gap-2 text-center items-center text-white cursor-pointer  py-2 px-3'
+}
+
+
 function App() {
   const [showHelp, setShowHelp] = useState(false)
+  const {helpButton} = buttonStyles
 
   return (
     <div className='app'>
-      <aside className=''>
-        <div className='flex gap-2 font-bold justify-center'>
-          <Button style='flex gap-2 text-center items-center bg-amber-200 hover:bg-amber-300 transition-all ease rounded-full py-2 px-3' icon={<HelpIcon />} label='Como usar' method={() => showHelpScreen(setShowHelp, showHelp)} />
+      <aside className='bg-slate-800'>
+        <div className='font-bold mx-auto hover:bg-linear-to-r rounded-lg bg-amber-500 hover:from-amber-500 hover:to-amber-300 transition ease duration-500'>
+          <Button style={helpButton} icon={<HelpIcon />} label='Como usar' method={() => showHelpScreen(setShowHelp, showHelp)} />
         </div>
         <h2 className='font-bold text-xl'>Menú de acciones</h2>
-        <Form submitAction={() => { }} />
+        <Form />
       </aside>
-      <main className='w-full'>
+      <main className='w-full' id='main'>
         {showHelp && <ShowHelp method={() => showHelpScreen(setShowHelp, showHelp)} />}
       </main>
     </div>
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,28 +1,90 @@
-interface Props {
-    submitAction: () => void
-}
+import { useState } from "react"
 
-export const Form = ({ submitAction }: Props) => {
-    const inputStyle = "flex flex-col items-center bg-slate-300 rounded-full w-3/4"
+export const Form = () => {
+    const inputStyle = "focus:text-black focus:border-black flex flex-col items-center rounded-lg cursor-pointer border border-slate-400 text-slate-400"
+    const [firstName, setFirstName] = useState('')
+    const [isAlive, setIsAlive] = useState('')
+    const [dob, setDob] = useState('')
+    const [dod, setDod] = useState('')
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        try {
+            if(dob == '') {
+                alert('Fecha de nacimiento invalida')
+                return
+            } else if (firstName == '' || typeof(firstName) == 'number') {
+                alert('Nombre no valido')
+            } else if (isAlive == 'No' && dod === '') {
+                alert('Fecha de defunción invalida')
+            } else {
+                const person = `
+                <ul class='flex flex-col p-4 text-black bg-slate-200 h-fit rounded-lg' draggable='true'>
+                    <li><b>Nombre:</b> ${firstName}</li>
+                    <li><b>¿Está vivo?</b> ${isAlive}</li>
+                    <li><b>Fecha de nacimiento:</b> ${dob}</li>
+                    <li><b>Fecha de defuncion:</b> ${dod}</li>
+                </ul>`;
+                document.querySelector('#main')?.insertAdjacentHTML('beforeend', person);
+            }
+        } catch (error) {
+            console.error(error)
+        }}
 
     return (
-        <form className="flex flex-col gap-4 justify-center items-center" onSubmit={submitAction}>
-            <input autoComplete="true" className="w-fit m-auto bg-slate-300 text-black placeholder:text-center text-center rounded-full p-2" type="text" name="name" placeholder="Nombre y apellido" />
-            <label htmlFor="alive" className="bg-slate-300 w-fit p-2 rounded-full">¿Está vivo?
-                <select name="alive" id='alive'>
-                    <option value="Si">Si</option>
+        <form className="flex flex-col gap-4 px-2" onSubmit={handleSubmit}>
+            <input
+                onChange={(e) => setFirstName(e.target.value)}
+                value={firstName}
+                autoComplete="true"
+                className=" focus:border-black text-black placeholder:text-center text-center border border-slate-400 rounded-lg p-2 cursor-pointer"
+                type="text"
+                id='name'
+                name="name"
+                placeholder="Nombre y apellido"
+            />
+            <label htmlFor="alive" className="p-2 rounded-lg border border-slate-400 text-slate-400 focus:text-black focus:border-black">
+                ¿Está vivo?
+                <select
+                    onChange={(e) => setIsAlive(e.target.value)}
+                    value={isAlive}
+                    name="alive"
+                    id='alive'
+                    className="cursor-pointer"
+                >
+                    <option value='Si'>Si</option>
                     <option value="No">No</option>
                 </select>
             </label>
             <label htmlFor="dob" className={inputStyle}>
                 Fecha de nacimiento
-                <input type="date" name="dob" id="dob" autoComplete="true" />
+                <input
+                    className="cursor-pointer"
+                    type="date"
+                    name="dob"
+                    id="dob"
+                    autoComplete="true"
+                    value={dob}
+                    onChange={(e) => setDob(e.target.value)}
+                />
             </label>
             <label htmlFor="dod" className={inputStyle}>
                 Fecha de defunción
-                <input type="date" name="dod" id="dod" autoComplete="true" />
+                <input
+                    className="cursor-pointer"
+                    type="date"
+                    name="dod"
+                    id="dod"
+                    autoComplete="true"
+                    value={dod}
+                    onChange={(e) => setDod(e.target.value)}
+                />
             </label>
-            <input type="submit" value="Enviar" className="bg-slate-300 rounded-full px-6 py-2" />
+            <input
+                type="submit"
+                value="Enviar"
+                className="bg-blue-600 text-white cursor-pointer rounded-lg px-6 py-2 hover:bg-linear-to-r hover:from-blue-500 hover:to-cyan-500  transition ease duration-500"
+            />
         </form>
     )
 }
